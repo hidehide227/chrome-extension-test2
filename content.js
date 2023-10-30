293d2c4af0a8b7e2bedc8576271c9b5af75c5ef0
hideaki227 <hideaki227@gmail.com>
2023-10-30 23:42:25 +0900

    23.10.30

--- content.js
+++ content.js
@@ -2,6 +2,34 @@ window.onload = function () {
     let e = this.document.createElement("h1");
     e.textContent = this.location.href;
     this.document.body.prepend(e);
-    console.log(this.location.href);
+    //
+    //
+    chrome.storage.local.get("urls", function (result) {
+        if (result.urls != null) {
+            let url = location.href;
+            let urls = result.urls;
+            if (!urls.includes(url)) {
+                urls.push(url);
+                let object = {
+                    urls: urls
+                }
+                chromeSet(object);
+            } else {
+                console.log(`duplicate:${url}`)
+            }
+
+        } else {
+            let object = {
+                urls: [location.href]
+            }
+            chromeSet(object)
+        }
+
+    });
+}
+function chromeSet(object) {
+    chrome.storage.local.set(object, function () {
+        console.log(`stored:${object.urls}`)
+    })
 }
 
--- popup.js
+++ popup.js
@@ -1,3 +1,18 @@
 window.onload = function () {
-    document.querySelector("#info").textContent = "aa"
+    document.querySelector("#btn_clear").addEventListener("click", function () {
+        chrome.storage.local.clear();
+    })
+    //
+    document.querySelector("#btn_print").addEventListener("click", function () {
+        chrome.storage.local.get("urls", function (result) {
+            if (result.urls != null) {
+                document.querySelector("#info1").textContent = `${result.urls.length}`;
+                let string = result.urls.join("-")
+                document.querySelector("#info2").textContent = string;
+            } else {
+                document.querySelector("#info1").textContent = `${0}`;
+                document.querySelector("#info2").textContent = "";
+            }
+        });
+    });
 }
